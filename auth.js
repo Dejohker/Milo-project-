8d11199c72a8f70d806441920130a3eb7f81b86a
ganiyu <olagunjuayodeleganiyu@gmail.com>
2026-06-15 12:41:21 +0100

    Enhance authentication form with password validation and button functionality

--- auth.js
+++ auth.js
@@ -1,9 +1,22 @@
 const form = document.getElementById("auth-form");
+const inputedPassword = document.getElementById("password")
+const btn = document.getElementById("btn")
 
+console.log(inputedPassword)
 if (form) {
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Login successful!");
-    window.location.href = "index.html";
-  });
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        if (inputedPassword.value.length < 6) {
+            return alert("Invalid password")
+        }
+
+        alert("Login successful!");
+        window.location.href = "index.html";
+    });
 }
+
+console.log(btn)
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    alert('Button is clicked')
+})
