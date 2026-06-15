5468b3101f1da66bb4f03a07725f9fa622afd4dd
Deola <adeolarazaq00@gmail.com>
2026-06-15 04:11:17 -0700

    Require login form fields

--- auth.js
+++ auth.js
@@ -3,9 +3,7 @@ const form = document.getElementById("auth-form");
 if (form) {
   form.addEventListener("submit", function (e) {
     e.preventDefault();
-
     alert("Login successful!");
-
     window.location.href = "index.html";
   });
 }
