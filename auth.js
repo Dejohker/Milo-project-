0e0dcbdd76f97cee34f7f153be65e3c4419d0753
Deola <adeolarazaq00@gmail.com>
2026-06-17 03:29:37 -0700

    Resolve auth updates and add products jump link

--- auth.js
+++ auth.js
@@ -1,22 +1,22 @@
 const form = document.getElementById("auth-form");
-const inputedPassword = document.getElementById("password")
-const btn = document.getElementById("btn")
+const inputedPassword = document.getElementById("password");
+const btn = document.getElementById("btn");
 
-console.log(inputedPassword)
 if (form) {
-    form.addEventListener("submit", function (e) {
-        e.preventDefault();
-        if (inputedPassword.value.length < 6) {
-            return alert("Invalid password")
-        }
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (inputedPassword.value.length < 6) {
+      return alert("Invalid password");
+    }
 
-        alert("Login successful!");
-        window.location.href = "index.html";
-    });
+    alert("Login successful!");
+    window.location.href = "index.html";
+  });
 }
 
-console.log(btn)
-btn.addEventListener("click", (e) => {
+if (btn) {
+  btn.addEventListener("click", (e) => {
     e.preventDefault();
-    alert('Button is clicked')
-})
+    alert("Button is successfully clicked");
+  });
+}
